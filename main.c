afa512bb415019b1dfee6526762a844a2a7eddf4
Marco RODRIGUES <marcoa_rodrigues@hotmail.com>
2018-09-11 10:21:42 +0200

    MAJ jeu LI

--- main.c
+++ main.c
@@ -1,28 +1,36 @@
+/*
+ ============================================================================
+ Name        : Lijeu.c
+ Author      : marco
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main()
-{
-    int x,i,j;
-    srand(time(NULL));
-    x= rand()%10+1;
-    for(i =0;i<3;i++)
-    {
-        printf("\n Enter the %d try :\n",(i+1));
-        scanf("%d",&j);
-        if(j>x)
-        {
-            printf("%d is greater than x\n",j);
-        }else if(j<x)
-         {
-            printf("%d is less than x\n",j);
-        }else
-         {
-            printf("%d = x \n YOU wins\n",j);
-            break;
-         }
-    }
-    printf("x=%d\n",x);
-    return 0;
+int main(void) {
+	 int x,i,j;
+	    srand(time(NULL));
+	    x= rand()%10+1;
+	    for(i =0;i<3;i++)
+	    {
+	        printf("\n Enter the %d try :\n",(i+1));
+	        scanf("%d",&j);
+	        if(j>x)
+	        {
+	            printf("%d is greater than x\n",j);
+	        }else if(j<x)
+	         {
+	            printf("%d is less than x\n",j);
+	        }else
+	         {
+	            printf("%d = x \n YOU wins\n",j);
+	            break;
+	         }
+	    }
+	    printf("x=%d\n",x);
+	    return 0;
 }
