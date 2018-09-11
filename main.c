354e951bd2fb1b40a12ffc87b8f25e4f04716ff6
formation <formation@formation7.si.c-s.fr>
2018-09-11 10:52:21 +0200

    two jeus !

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 int main()
 {
     int x,i,j;
     srand(time(NULL));
     x= rand()%10+1;
+    printf("\n x in [1,10]\n");
     for(i =0;i<3;i++)
     {
         printf("\n Enter the %d try :\n",(i+1));
@@ -24,5 +24,28 @@ int main()
          }
     }
     printf("x=%d\n",x);
+
+    printf("\n------------------------------------------------------------ \n");
+    printf("\n now x in [1,1000]\n");
+    srand(time(NULL));
+    x= rand()%1000+1;
+    for(i =0;i<10;i++)
+    {
+        printf("\n Enter the %d try :\n",(i+1));
+        scanf("%d",&j);
+        if(j>x)
+        {
+            printf("%d is greater than x\n",j);
+        }else if(j<x)
+         {
+            printf("%d is less than x\n",j);
+        }else
+         {
+            printf("%d = x \n YOU win\n",j);
+            break;
+         }
+    }
+    printf("x=%d\n",x);
+
     return 0;
 }
